2414f854c92d73633f85f5dd62051758b0fca7dc
swordgeo <swordgeo1094@yahoo.com>
2023-01-25 16:02:28 -0500

    typo

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -2,7 +2,7 @@ const checkLogin = (req, res, next) => {
   if (!req.oidc.isAuthenticated()) {
     return res.status(401).send({
       error: 'Not authorized to change data without login.',
-      login: "https://cse341-l7-onrender.com/login"
+      login: "https://cse341-l7.onrender.com/login"
     });
   }
   next();
